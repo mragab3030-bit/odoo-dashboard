d96ba16c342d5dc844c3ccdceab4071a5e14c5e0
Mohamed Ragab <mohamedragab@Mohameds-MacBook-Air.local>
2026-05-10 13:51:11 +0300

    fix port binding for render

--- app.py
+++ app.py
@@ -219,4 +219,5 @@ def export_pdf():
                      download_name='invoices_' + today + '.pdf')
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=False)
